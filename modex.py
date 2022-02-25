eddd7b4653babff2705ccdbf85f80353fce66be1
Pedro <pedrofernandezalvarez6@gmail.com>
2022-02-25 10:52:59 +0100

    Retrieve details about the pages of each dumped module

--- modex.py
+++ modex.py
@@ -26,14 +26,58 @@ def create_logger(filename: str):
     return modex_logger
 
 
-def create_module_representation(name: str, base_address: str, size: str, process_id: int,
-                                 filename: str) -> Dict[str, Any]:
-    return {'name': name, 'base_address': base_address, 'size': size, 'process_id': process_id,
-            'filename': filename}
+def get_relevant_page_details(page: List[Any]) -> Dict[str, str]:
+    relevant_page_details = {}
+    page_details = page[3]
+
+    page_vaddr_match = re.search(r'\nvaddr: (\w+)\n', page_details)
+    if page_vaddr_match:
+        page_vaddr = page_vaddr_match.group(1)
+    else:
+        page_vaddr = None
+
+    page_length_match = re.search(r'\nlength: (\w+)\n', page_details)
+    if page_length_match:
+        page_length = page_length_match.group(1)
+    else:
+        page_length = None
+
+    page_pfn_db_entry_prototype_pte_flag_match = re.search(r'\nhas_proto_set: (\w+)\n', page_details)
+    if page_pfn_db_entry_prototype_pte_flag_match:
+        page_pfn_db_entry_prototype_pte_flag = page_pfn_db_entry_prototype_pte_flag_match.group(1)
+    else:
+        page_pfn_db_entry_prototype_pte_flag = None
+
+    relevant_page_details['virtual_address'] = page_vaddr
+    relevant_page_details['size'] = page_length
+    relevant_page_details['pfn_db_entry_prototype_pte_flag'] = page_pfn_db_entry_prototype_pte_flag
+    return relevant_page_details
 
 
 log_filename = create_log_filename()
 logger = create_logger(log_filename)
+
+
+class Page:
+    def __init__(self, virtual_address: str, size: str, pfn_db_entry_prototype_pte_flag: str):
+        self.virtual_address: str = virtual_address
+        self.size: str = size
+        # pfn_db_entry_prototype_pte_flag can be 'True', 'False', or 'Undetermined'
+        self.pfn_db_entry_prototype_pte_flag: str = pfn_db_entry_prototype_pte_flag
+
+
+class Module:
+    def __init__(self, name: str, base_address: str, size: str, process_id: int, filename: str, pages: List[Page]):
+        self.name: str = name
+        self.base_address: str = base_address  # Virtual base address
+        self.size: str = size  # Size in bytes
+        self.process_id: int = process_id  # Identifier of the process where the module is mapped
+        self.filename: str = filename  # Filename of the dumped module
+        self.pages: List[Page] = pages
+
+    def get_basic_information(self):
+        return {'name': self.name, 'base_address': self.base_address, 'size': self.size, 'process_id': self.process_id,
+                'filename': self.filename, 'number_of_retrieved_pages': len(self.pages)}
 
 
 class Modex(interfaces.plugins.PluginInterface):
@@ -65,9 +109,10 @@ class Modex(interfaces.plugins.PluginInterface):
 
     def run(self):
         module_supplied: str = self.config['module'].casefold()
-        modules_to_mix: List[Dict[str, Any]] = []
+        modules_to_mix: List[Module] = []
         files_finally_generated: List[str] = [log_filename]
 
+        # For each process, find if the supplied module is mapped on it. If so, dump the module.
         processes = pslist.PsList.list_processes(self.context,
                                                  self.config['primary'],
                                                  self.config['nt_symbols'])
@@ -77,6 +122,7 @@ class Modex(interfaces.plugins.PluginInterface):
                                                                 'windows',
                                                                 'pe',
                                                                 class_types=pe.class_types)
+
         for process in processes:
             process_id = process.UniqueProcessId
             process_layer_name = process.add_process_layer()
@@ -104,24 +150,70 @@ class Modex(interfaces.plugins.PluginInterface):
                             if file_handle:
                                 file_handle.close()
                                 module_filename = file_handle.preferred_filename
-                                modules_to_mix.append(create_module_representation(module_name,
-                                                                                   hex(module_base_address),
-                                                                                   hex(module_size),
-                                                                                   process_id,
-                                                                                   module_filename))
+                                modules_to_mix.append(Module(module_name,
+                                                             hex(module_base_address),
+                                                             hex(module_size),
+                                                             process_id,
+                                                             module_filename, []))
                 except exceptions.InvalidAddressException:
                     pass
 
+        # For each dumped module, retrieve information about its pages
+        for module_to_mix in modules_to_mix:
+            pages: List[List[Any]] = self.get_module_pages(module_to_mix)
+            for page in pages:
+                relevant_page_details: Dict[str, str] = get_relevant_page_details(page)
+                if relevant_page_details['virtual_address'] is not None and relevant_page_details[
+                    'size'] is not None and relevant_page_details['pfn_db_entry_prototype_pte_flag'] is not None:
+                    module_to_mix.pages.append(Page(relevant_page_details['virtual_address'],
+                                                    relevant_page_details['size'],
+                                                    relevant_page_details['pfn_db_entry_prototype_pte_flag']
+                                                    ))
+
+        # Check if the last page retrieved for each module is out of bounds
+        for module_to_mix in modules_to_mix:
+            first_out_of_bounds_address: int = int(module_to_mix.base_address, 16) + int(module_to_mix.size, 16)
+            if int(module_to_mix.pages[-1].virtual_address, 16) == first_out_of_bounds_address:
+                del module_to_mix.pages[-1]
+
         logger.info(f'Modules to mix ({len(modules_to_mix)}):')
         for module_to_mix in modules_to_mix:
-            logger.info(f'\t{module_to_mix}')
+            logger.info(f'\t{module_to_mix.get_basic_information()}')
+
+        logger.info(f'\nModules to mix (alongside the retrieved pages for each one):')
+        for module_to_mix in modules_to_mix:
+            logger.info(f'\t{module_to_mix.get_basic_information()}')
+            for page in module_to_mix.pages:
+                logger.info(f'\t\t{vars(page)}')
 
         # Delete the .dmp files that were used to create the final .dmp file
         for module_to_mix in modules_to_mix:
-            os.remove(module_to_mix['filename'])
+            os.remove(module_to_mix.filename)
 
         return renderers.TreeGrid([("Filename", str)], self._generator(files_finally_generated))
 
     def _generator(self, filenames):
         for filename in filenames:
             yield 0, [filename]
+
+    def get_module_pages(self, module: Module) -> List[List[Any]]:
+        pages: List[List[Any]] = []
+        module_base_address: int = int(module.base_address, 16)
+        self.context.config['plugins.Modex.SimplePteEnumerator.pid'] = [module.process_id]
+        self.context.config['plugins.Modex.SimplePteEnumerator.start'] = module_base_address
+        self.context.config['plugins.Modex.SimplePteEnumerator.end'] = module_base_address + int(module.size, 16)
+        self.context.config['plugins.Modex.SimplePteEnumerator.include_image_files'] = True
+        self.context.config['plugins.Modex.SimplePteEnumerator.check_valid'] = True
+        self.context.config['plugins.Modex.SimplePteEnumerator.print_pages'] = True
+
+        automagics = automagic.choose_automagic(automagic.available(self._context), simple_pteenum.SimplePteEnumerator)
+        simple_pteenum_plugin = plugins.construct_plugin(self.context, automagics, simple_pteenum.SimplePteEnumerator,
+                                                         self.config_path, self._progress_callback, self.open)
+        treegrid = simple_pteenum_plugin.run()
+
+        def visitor(node, _accumulator):
+            pages.append(node.values)
+            return None
+
+        treegrid.populate(visitor, None)
+        return pages
